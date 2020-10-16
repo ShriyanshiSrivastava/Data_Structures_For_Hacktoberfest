fac83e7b0c79b4c1f1ffdd7f62a923bc5d5ca3cc
utsav7011 <utsav7011@gmail.com>
2020-10-16 22:48:34 +0530

    updated hii

--- hii.cpp
+++ hii.cpp
@@ -1,8 +1,20 @@
-#include<stdio.h>
+#include<iostream>
+
+#include<conio.h>
+#include<windows.h>
+
 using namespace std;
+
+void gotoxy(short x, short y)           //definition of gotoxy function//
+{
+ COORD pos ={x,y};
+ SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
 int main()
 {
+       gotoxy(50,0);
     cout<<"Hello World";
+     gotoxy(48,1);
     cout<<"new line added";
     return 0;
 }
