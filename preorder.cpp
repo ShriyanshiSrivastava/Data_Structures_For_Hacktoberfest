91c126f2e82f883a2ef4f0b53c841983a34b8e5b
SHAZIA SALIM <54541275+s2278@users.noreply.github.com>
2020-10-19 21:20:19 +0530

    Add files via upload

---
+++ preorder.cpp
@@ -0,0 +1,63 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+struct btnode
+{
+    int data;
+    struct btnode *left;
+    struct btnode *right;
+};
+struct btnode *root;
+struct btnode* createbtnode(int n)
+{
+    struct btnode *node= (struct btnode*)malloc(sizeof (struct btnode));
+    node->data=n;
+    node->left=NULL;
+    node->right=NULL;
+    return node;
+    
+}
+void preorder(struct btnode *root)
+{
+    if(root)
+    {
+        printf("%d",root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+int main()
+{
+   int ch,n,c;
+   while(1)
+   {
+   printf("1.enter elements to tree\n2.preorder traversal.\n6.exit\n");
+   printf("enter your choice\n");
+   scanf("%d",&ch);
+   switch(ch)
+   {
+       case 1: root=createbtnode(1);
+       root->left=createbtnode(2);
+       root->right=createbtnode(3);
+       root->left->left=createbtnode(4);
+       root->left->right=createbtnode(5);
+       break;
+       case 2: preorder(root);
+       printf("\n");
+       break;
+       case 6:exit(0);
+       break;
+       default:printf("invalid choice\n");
+   }
+   }
+
+    return 0;
+}
