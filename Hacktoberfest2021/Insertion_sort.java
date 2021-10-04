5890b52bef3e885fdb270da89b529d7ff2c12cc1
Hitarth Srivastava <srivastavahitarth19@gmail.com>
2021-10-04 14:49:53 +0530

    Adding Insertion sort(java)

---
+++ Hacktoberfest2021/Insertion_sort.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {3,1,5,2,4};
+        insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void insertionSort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0 ; j--) {
+                if(arr[j] < arr[j-1]){
+                    swap(arr, j,j-1);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+    }
+    static void swap(int[] arr,int a,int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
