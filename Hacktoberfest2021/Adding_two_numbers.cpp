a8c0183b25e3366a8bdb2cb80400af1bcee95030
Prakash Singh Rautela <56201964+OptiMus-cyber@users.noreply.github.com>
2021-10-06 00:54:19 +0530

    Program to add two numbers

---
+++ Hacktoberfest2021/Adding_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2,sum;
+    cout<<"n1:";
+    cin>>n1;
+    cout<<"n2:";
+    cin>>n2;
+    sum=n1+n2;
+    cout<<"sum:"<<sum;
+    return 0;
+}
